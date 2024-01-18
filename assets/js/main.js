23f2736d855f068d061c0eec9ccfd18bde54971d
Matt Panyor <matt@panyor.com>
2024-01-18 18:05:32 +0000

    adding business hours

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     // page setup  -- START --
 
+    const BUSINESS_HOURS = [9,10,11,12,13,14,15,16,17];
+
     const TODAY = dayjs().format('DD MMMM YYYY');
     const TIME = dayjs().format('HH:mm');
     $('#currentDay').text(TODAY + ' [' + TIME + ']');
